a338a7997ca2d34a2f091be7da5dfdc50be9a928
davidvilanmiguens <dvilanmiguens@danielcastelao.org>
2021-05-13 14:08:17 +0200

    clase main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,14 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        DatosDeEntrada unaConsola = FactoriaDeEntrada.get(1);
+        unaConsola.introducirDatos();
+
+        DatosDeSalida unaventana = FactoriaDeSalida.getProducto(2,"hola");
+        unaventana.visualizar();
+
+        DatosDeSalida misalidat;
+    }
+}
     }
 }
